4d46225589599e86d7edac83c967ac36cd61826a
Maisan Auliya <maisanaulia02@gmail.com>
2025-10-14 11:37:34 +0700

    updating git ignore

---
+++ test/tapper/domain/entities/fake_tapper.mock.dart
@@ -0,0 +1,4 @@
+import 'package:flutter_clean_tdd_template/src/features/tapper/domain/entities/tap_per_day.dart';
+import 'package:mocktail/mocktail.dart';
+
+class FakeTapPerDay extends Fake implements TapPerDay {}
