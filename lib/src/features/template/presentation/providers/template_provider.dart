36011dc5b4dc1679528379f99a5fb9a6b38d31b5
Maisan Auliya <maisanaulia02@gmail.com>
2025-10-17 14:03:07 +0700

    update feat: clearing dependecies from presentation layer and data layer

--- lib/src/features/template/presentation/providers/template_provider.dart
+++ lib/src/features/template/presentation/providers/template_provider.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/cupertino.dart';
 
-import '../../data/models/template_version_model.dart';
 import '../../domain/entities/template_version.dart';
 
 class TemplateProvider extends ChangeNotifier {
-  TemplateVersionModel? _templateVersion;
+  TemplateVersion? _templateVersion;
 
-  TemplateVersionModel? get templateVersion => _templateVersion;
+  TemplateVersion? get templateVersion => _templateVersion;
 
   void init() {
     _templateVersion = null;
   }
 
   void updateTemplateVersion(TemplateVersion version) {
-    _templateVersion = TemplateVersionModel.fromEntity(version);
+    _templateVersion = version;
     notifyListeners();
   }
 }
